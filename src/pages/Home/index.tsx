f08577b5bed80484583994891e26eae1532e01d4
gramedcart <anbulbay@gmail.com>
2023-01-31 16:46:45 +0900

    chore: add signal to homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,28 @@
+import { createSignal, For, onMount } from "solid-js"
 import CharacterCard from "../../shared/CharacterCard"
 import CreateNewCard from "./components/CreateNewCard"
 
-
 const HomePage = () => {
+    const [myCharacters, setMyCharacters] = createSignal([] as Character[]);
+    const [communityCharacters, setCommunityCharacters] = createSignal([] as Character[]);
+
     return <>
         <h1 class="text-3xl">Your Characters</h1>
         <p class="text-white/50">Characters that you made or imported.</p>
         <div class="my-4 border-b border-white/5" />
         <div class="flex flex-wrap gap-4 justify-center w-full">
-            <CharacterCard displayName="MyVeryCuteWaifu" avatarUrl="https://via.placeholder.com/150" />
-            <CharacterCard displayName="VeryCuteWaifu" avatarUrl="https://via.placeholder.com/150" />
-            <CharacterCard displayName="VeryCuteWaifu" avatarUrl="https://via.placeholder.com/150" />
-            <CharacterCard displayName="VeryCuteWaifu" avatarUrl="https://via.placeholder.com/150" />
+            <For each={myCharacters()}>{(e:Character) => {
+                return <CharacterCard displayName={e.name} avatarUrl={e.avatarUrl} />
+            }}</For>
             <CreateNewCard />
         </div>
         <h1 class="text-3xl mt-10">Community Characters</h1>
         <p class="text-white/50">Characters that you made by the community.</p>
         <div class="my-4 border-b border-white/5" />
         <div class="flex flex-wrap gap-4 justify-center">
-            <CharacterCard displayName="MyVeryCuteWaifu" avatarUrl="https://via.placeholder.com/150" />
-            <CharacterCard displayName="VeryCuteWaifu" avatarUrl="https://via.placeholder.com/150" />
-            <CharacterCard displayName="VeryCuteWaifu" avatarUrl="https://via.placeholder.com/150" />
-            <CharacterCard displayName="VeryCuteWaifu" avatarUrl="https://via.placeholder.com/150" />
-
+        <For each={communityCharacters()}>{(e:Character) => {
+                return <CharacterCard displayName={e.name} avatarUrl={e.avatarUrl} />
+            }}</For>
         </div>
     </>
 }
--- src/shared/CharacterCard.tsx
+++ src/shared/CharacterCard.tsx
@@ -1,6 +1,6 @@
 import { Component, Show } from "solid-js";
 
-const CharacterCard: Component<{ displayName: string, avatarUrl:string }> = (props) => {
+const CharacterCard: Component<{ displayName: string, avatarUrl?:string }> = (props) => {
     return <div
         style={{"background-image": `url("${props.avatarUrl}")`}}
         class="bg-white rounded-lg h-40 w-40 bg-cover drop-shadow-[0_1px_2px_rgba(255,255,255,0.1)] relative transition hover:-translate-y-1 hover:scale-105">
