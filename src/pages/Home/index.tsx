0e1f8811dc6991a38032e9ea78aefac7d626826f
gramedcart <anbulbay@gmail.com>
2023-01-31 17:24:43 +0900

    feat: rework homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,24 +3,73 @@ import CharacterCard from "../../shared/CharacterCard"
 import CreateNewCard from "./components/CreateNewCard"
 
 const HomePage = () => {
-    const [myCharacters, setMyCharacters] = createSignal([] as Character[]);
-    const [communityCharacters, setCommunityCharacters] = createSignal([] as Character[]);
+    const testTemplate = [
+        {
+            name: 'Ibuki-chan',
+            avatarUrl: '',
+            description: ''
+        },
+        {
+            name: 'Ibuki-chan',
+            avatarUrl: '',
+            description: ''
+        },
+        {
+            name: 'Ibuki-chan',
+            avatarUrl: '',
+            description: ''
+        },
+        {
+            name: 'Ibuki-chan',
+            avatarUrl: '',
+            description: ''
+        },
+        {
+            name: 'Ibuki-chan',
+            avatarUrl: '',
+            description: ''
+        }
+    ] as Character[]
+
+
+    const [recentlyChatedCharacters, setRecentlyChatedCharacters] = createSignal(testTemplate);
+    const [recommendedCharacters, setRecommendedCharacters] = createSignal(testTemplate);
+    const [popularCharacters, setPopularCharacters] = createSignal(testTemplate);
+    const [recentlyCreatedCharacters, setRecentlyCreatedCharacters] = createSignal(testTemplate);
 
     return <>
-        <h1 class="text-3xl">Your Characters</h1>
-        <p class="text-white/50">Characters that you made or imported.</p>
-        <div class="my-4 border-b border-white/5" />
-        <div class="flex flex-wrap gap-4 justify-center w-full">
-            <For each={myCharacters()}>{(e:Character) => {
-                return <CharacterCard displayName={e.name} avatarUrl={e.avatarUrl} />
-            }}</For>
-            <CreateNewCard />
-        </div>
-        <h1 class="text-3xl mt-10">Community Characters</h1>
-        <p class="text-white/50">Characters that you made by the community.</p>
+        <h1 class="text-3xl mt-10">Recently Chatted Characters</h1>
+        <p class="text-white/50">Characters that you recently chatted.</p>
         <div class="my-4 border-b border-white/5" />
         <div class="flex flex-wrap gap-4 justify-center">
-        <For each={communityCharacters()}>{(e:Character) => {
+            <For each={recentlyChatedCharacters()}>{(e:Character) => {
+                return <CharacterCard displayName={e.name} avatarUrl={e.avatarUrl} />
+            }}</For>
+        </div>
+
+        <h1 class="text-3xl mt-10">Recommended Characters</h1>
+        <p class="text-white/50">Characters that are recommended for you</p>
+        <div class="my-4 border-b border-white/5" />
+        <div class="flex flex-wrap gap-4 justify-center">
+        <For each={recommendedCharacters()}>{(e:Character) => {
+                return <CharacterCard displayName={e.name} avatarUrl={e.avatarUrl} />
+            }}</For>
+        </div>
+
+        <h1 class="text-3xl mt-10">Popular characters</h1>
+        <p class="text-white/50">Characters that are popular in the community.</p>
+        <div class="my-4 border-b border-white/5" />
+        <div class="flex flex-wrap gap-4 justify-center">
+        <For each={popularCharacters()}>{(e:Character) => {
+                return <CharacterCard displayName={e.name} avatarUrl={e.avatarUrl} />
+            }}</For>
+        </div>
+
+        <h1 class="text-3xl mt-10">Recently Created Characters</h1>
+        <p class="text-white/50">Characters that are made recently.</p>
+        <div class="my-4 border-b border-white/5" />
+        <div class="flex flex-wrap gap-4 justify-center">
+        <For each={recentlyCreatedCharacters()}>{(e:Character) => {
                 return <CharacterCard displayName={e.name} avatarUrl={e.avatarUrl} />
             }}</For>
         </div>
--- src/shared/CharacterCard.tsx
+++ src/shared/CharacterCard.tsx
@@ -4,8 +4,9 @@ const CharacterCard: Component<{ displayName: string, avatarUrl?:string }> = (pr
     return <div
         style={{"background-image": `url("${props.avatarUrl}")`}}
         class="bg-white rounded-lg h-40 w-40 bg-cover drop-shadow-[0_1px_2px_rgba(255,255,255,0.1)] relative transition hover:-translate-y-1 hover:scale-105">
-        
-        <span class="absolute bottom-2 left-3 drop-shadow-lg text-base">{props.displayName}</span>
+        <div class="bg-gradient-to-t from-gray-700 w-full h-full rounded-lg">
+        </div>
+        <span class="absolute bottom-2 left-3 text-shadow">{props.displayName}</span>
     </div>
 }
 
